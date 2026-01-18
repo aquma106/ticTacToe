b829820127c173300b6c3f305ebc3508332c5595
aquma106 <thapahemraj141@gmail.com>
2026-01-18 17:32:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import "./App.css";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board() {
+  const [isNext, setIsNext] = useState(true);
+  const [square, setSquare] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (square[i]) return;
+    const nextSquare = square.slice();
+    if (isNext) {
+      nextSquare[i] = "X";
+    } else {
+      nextSquare[i] = "O";
+    }
+
+    setSquare(nextSquare);
+    setIsNext(!isNext);
+  }
+  return (
+    <>
+      <div className="board-row">
+        <Square
+          value={square[0]}
+          onSquareClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={square[1]}
+          onSquareClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={square[2]}
+          onSquareClick={() => {
+            handleClick(2);
+          }}
+        />
+      </div>
+      <div className="board-row">
+        <Square
+          value={square[3]}
+          onSquareClick={() => {
+            handleClick(3);
+          }}
+        />
+        <Square
+          value={square[4]}
+          onSquareClick={() => {
+            handleClick(4);
+          }}
+        />
+        <Square
+          value={square[5]}
+          onSquareClick={() => {
+            handleClick(5);
+          }}
+        />
+      </div>
+      <div className="board-row">
+        <Square
+          value={square[6]}
+          onSquareClick={() => {
+            handleClick(6);
+          }}
+        />
+        <Square
+          value={square[7]}
+          onSquareClick={() => {
+            handleClick(7);
+          }}
+        />
+        <Square
+          value={square[8]}
+          onSquareClick={() => {
+            handleClick(8);
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Board;
